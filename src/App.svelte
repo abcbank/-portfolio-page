ed3d8a7044f03b7cb82114806828b40a4bf509f0
abcbank <woejr_2012@naver.com>
2023-01-18 17:14:24 +0900

    봄 이펙트 추가

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,49 @@
-<script>
-    import Navbar from "./component/Navbar.svelte";
-	export let name;
-</script>
 
+<script>
+    import { onMount } from "svelte";
+    import Navbar from "./component/Navbar.svelte";
+	import Snowflakes from "magic-snowflakes"
+	export let name;
+
+	onMount(() => {		
+		let month = new Date().getMonth() + 1;
+		let season = "";
+		let field;
+		
+		if(3 <= month && month <= 5){
+			var sakura = new Sakura('main');
+			season = "Spring";
+		}
+		else if(6 <= month && month <= 8){
+			season = "Summer"
+		}
+		else if(9 <= month && month <= 11){
+			season = "Fall"
+		}
+		else{
+			var sf = new Snowflakes
+			({
+				count:100
+			});
+			season = "Winter"
+		}
+	})
+</script>
 <main>
+	
 	<Navbar />
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div id="details">
+	</div>
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
+		max-width: 240px;
+		height:100vh;
+		background: #000;
+		overflow:hidden;
 	}
-
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
--- src/component/Navbar.svelte
+++ src/component/Navbar.svelte
@@ -1,96 +1,96 @@
 <script lang="ts">
-    import {
-      Collapse,
-      Navbar,
-      NavbarToggler,
-      NavbarBrand,
-      Nav,
-      NavItem,
-      NavLink,
-      Dropdown,
-      DropdownToggle,
-      DropdownMenu,
-      DropdownItem
-    } from 'sveltestrap';
-    
-    let toggleEnabled = false;
-    let isOpen_Project = false;
-    let isOpen_Outsourcing = false;
+  import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+  } from 'sveltestrap';
+  
+  let toggleEnabled = false;
+  let isOpen_Project = false;
+  let isOpen_Outsourcing = false;
 
-    function handleUpdate(event) {
-      if(ProjectMouseup)
-        isOpen_Project = event.detail.isOpen;
-      if(OutsourcingMouseUp)
-        isOpen_Outsourcing = event.detail.isOpen;
-    }
-    let ProjectMouseup = false;
-    let OutsourcingMouseUp = false;
-    function ProjectOpenMouseUp(e) {
-      ProjectMouseup = true;
-    }
-    function ProjectOpenMouseOut(e) {
-      ProjectMouseup = false;
-    }
-    function OutsourcingOpenMouseUp(e) {
-      OutsourcingMouseUp = true;
-    }
-    function OutsourcingOpenMouseOut(e) {
-      OutsourcingMouseUp = false;
-    }
+  function handleUpdate(event) {
+    if(ProjectMouseup)
+      isOpen_Project = event.detail.isOpen;
+    if(OutsourcingMouseUp)
+      isOpen_Outsourcing = event.detail.isOpen;
+  }
+  let ProjectMouseup = false;
+  let OutsourcingMouseUp = false;
+  function ProjectOpenMouseUp(e) {
+    ProjectMouseup = true;
+  }
+  function ProjectOpenMouseOut(e) {
+    ProjectMouseup = false;
+  }
+  function OutsourcingOpenMouseUp(e) {
+    OutsourcingMouseUp = true;
+  }
+  function OutsourcingOpenMouseOut(e) {
+    OutsourcingMouseUp = false;
+  }
 
-  </script>
+</script>
 
-  <div class="NavBar">
-    <Navbar color="dark" dark expand="md">
-      <NavbarBrand href="/">
-        <img class="logo" alt="logo" src="assets/image/icon.png" />
-        <span class="logo-text">
-          abcBank
-        </span>
-      </NavbarBrand>
-      <NavbarToggler on:click={() => (toggleEnabled = !toggleEnabled)} />
-      <Collapse { toggleEnabled } navbar expand="md" on:update={handleUpdate}>
-        <Nav class="ms-auto" navbar>
-          <NavItem>
-            <NavLink href="#components/">About Me</NavLink>
-          </NavItem>
-          
-          <Dropdown nav inNavbar>
-            <DropdownToggle nav class="nav-link" caret on:mouseover={ProjectOpenMouseUp} on:mouseout={ProjectOpenMouseOut}>Projects</DropdownToggle>
-            <DropdownMenu color="dark" dark right>
-              <DropdownItem>2022</DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem>2023</DropdownItem>
-              <!-- <DropdownItem divider />
-              <DropdownItem>Reset</DropdownItem> -->
-            </DropdownMenu>
-          </Dropdown>
+<div class="NavBar">
+  <Navbar color="dark" dark expand="md">
+    <NavbarBrand href="/">
+      <img class="logo" alt="logo" src="assets/image/icon.png" />
+      <span class="logo-text">
+        abcBank
+      </span>
+    </NavbarBrand>
+    <NavbarToggler on:click={() => (toggleEnabled = !toggleEnabled)} />
+    <Collapse { toggleEnabled } navbar expand="md" on:update={handleUpdate}>
+      <Nav class="ms-auto" navbar>
+        <NavItem>
+          <NavLink href="#components/">About Me</NavLink>
+        </NavItem>
+        
+        <Dropdown nav inNavbar>
+          <DropdownToggle nav class="nav-link" caret on:mouseover={ProjectOpenMouseUp} on:mouseout={ProjectOpenMouseOut}>Projects</DropdownToggle>
+          <DropdownMenu color="dark" dark right>
+            <DropdownItem>2022</DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem>2023</DropdownItem>
+            <!-- <DropdownItem divider />
+            <DropdownItem>Reset</DropdownItem> -->
+          </DropdownMenu>
+        </Dropdown>
 
-          <Dropdown nav inNavbar>
-            <DropdownToggle nav class="nav-link" caret on:mouseover={OutsourcingOpenMouseUp} on:mouseout={OutsourcingOpenMouseOut}>Outsourcing</DropdownToggle>
-            <DropdownMenu  right>
-              <DropdownItem>2022</DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem>2023</DropdownItem>
-              <!-- <DropdownItem divider />
-              <DropdownItem>Reset</DropdownItem> -->
-            </DropdownMenu>
-          </Dropdown>
-          
-          <NavItem>
-            <NavLink href="https://github.com/abcbank">GitHub</NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
-    </Navbar>
-  </div>
+        <Dropdown nav inNavbar>
+          <DropdownToggle nav class="nav-link" caret on:mouseover={OutsourcingOpenMouseUp} on:mouseout={OutsourcingOpenMouseOut}>Outsourcing</DropdownToggle>
+          <DropdownMenu  right>
+            <DropdownItem>2022</DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem>2023</DropdownItem>
+            <!-- <DropdownItem divider />
+            <DropdownItem>Reset</DropdownItem> -->
+          </DropdownMenu>
+        </Dropdown>
+        
+        <NavItem>
+          <NavLink href="https://github.com/abcbank">GitHub</NavLink>
+        </NavItem>
+      </Nav>
+    </Collapse>
+  </Navbar>
+</div>
 
-  <style>
-    .logo {
-      width:30px; 
-      height:30px;
-    }
-    .logo-text{
-      padding-left:10px;
-    }
-  </style>
+<style>
+  .logo {
+    width:30px; 
+    height:30px;
+  }
+  .logo-text{
+    padding-left:10px;
+  }
+</style>
