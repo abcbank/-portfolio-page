8d8d86f8b4e7c7a289f6ee2ffb0a9e6992b70c66
abcbank <woejr_2012@naver.com>
2023-02-13 16:18:28 +0900

    te

--- src/component/Sidebar/Sidebar.svelte
+++ src/component/Sidebar/Sidebar.svelte
@@ -30,7 +30,7 @@
         background-color:#282a35;
         top:60px;
         width:40%;
-        min-width: 120px;
+        min-width: 130px;
         height:100%;
 		right: -40%;
 		transition: right 0.3s ease-in-out
