eb941629e9a9b59c6acc2754e38ab0e42b77bbb7
abcbank <woejr_2012@naver.com>
2023-02-14 10:20:22 +0900

    te

--- src/component/PageDesign/Slide/2021/Projects/Downloader/Slide02/Page02.svelte
+++ src/component/PageDesign/Slide/2021/Projects/Downloader/Slide02/Page02.svelte
@@ -9,17 +9,16 @@
 </script>
 
 <DefaultPage color={color} fontSize={fontSize}>
-    <p>프로젝트 목적</p>
+    <p style="font-size:{fontSize}rem;">프로젝트 목적</p>
     <div style="font-size:{calcSubFontSize()}rem; text-align:left; margin-top:20px;">
         <p>
-            
-            - PCB에 펌웨어를 넣는 작업을 자동화<br/>
-            - PCB에 대해 지정된 검사를 진행해 PCB의 불량 여부를 사전 파악
-            <!-- {#if $Device["isMobile"]}
+            {#if $Device["isMobile"]}
             - PCB에 펌웨어를 넣는 작업을 자동화<br/>
             - PCB에 대해 지정된 검사를 진행해<br/>PCB의 불량 여부를 사전 파악
             {:else}
-            {/if} -->
+            - PCB에 펌웨어를 넣는 작업을 자동화<br/>
+            - PCB에 대해 지정된 검사를 진행해 PCB의 불량 여부를 사전 파악
+            {/if}
         </p>
     </div>
 </DefaultPage>
