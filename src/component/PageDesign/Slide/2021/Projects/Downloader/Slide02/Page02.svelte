9c73d106535268ff4fa164de435b2ad6b2efb722
abcbank <woejr_2012@naver.com>
2023-02-14 10:16:43 +0900

    te

--- src/component/PageDesign/Slide/2021/Projects/Downloader/Slide02/Page02.svelte
+++ src/component/PageDesign/Slide/2021/Projects/Downloader/Slide02/Page02.svelte
@@ -3,7 +3,7 @@
     import DefaultPage from '../../../../defaultPage.svelte'
 
     export let color = "transparent";
-    export let fontSize = 2
+    export let fontSize = $Device['isMobile'] ? 1 : 2;
 
     const calcSubFontSize = () => !$Device["isMobile"] ? fontSize : fontSize/2;
 </script>
@@ -12,8 +12,14 @@
     <p>프로젝트 목적</p>
     <div style="font-size:{calcSubFontSize()}rem; text-align:left; margin-top:20px;">
         <p>
+            
             - PCB에 펌웨어를 넣는 작업을 자동화<br/>
             - PCB에 대해 지정된 검사를 진행해 PCB의 불량 여부를 사전 파악
+            <!-- {#if $Device["isMobile"]}
+            - PCB에 펌웨어를 넣는 작업을 자동화<br/>
+            - PCB에 대해 지정된 검사를 진행해<br/>PCB의 불량 여부를 사전 파악
+            {:else}
+            {/if} -->
         </p>
     </div>
 </DefaultPage>
