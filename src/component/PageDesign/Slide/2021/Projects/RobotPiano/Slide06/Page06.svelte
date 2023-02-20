b39b45171d3bfc74fa28106bc5fa137061d33c40
abcbank <woejr_2012@naver.com>
2023-02-20 18:51:15 +0900

    te

--- src/component/PageDesign/Slide/2021/Projects/RobotPiano/Slide06/Page06.svelte
+++ src/component/PageDesign/Slide/2021/Projects/RobotPiano/Slide06/Page06.svelte
@@ -12,7 +12,12 @@
     <p>아두이노의 성능 저하</p>
     <div style="font-size:{context}rem; text-align:left; margin-top:20px;">
         <p>
+            {#if $Device["isMobile"]}
+            - 아두이노의 반응속도로 인해 제때 실린더가 내려가지<br/>
+            &nbsp;&nbsp;않음
+            {:else}
             - 아두이노의 반응속도로 인해 제때 실린더가 내려가지 않음
+            {/if}
         </p>
     </div>
 </DefaultPage>
