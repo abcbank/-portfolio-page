b854a6a875e90a37ad60e555c6157f1020fb1b71
abcbank <woejr_2012@naver.com>
2023-02-20 19:30:55 +0900

    te

--- src/component/PageDesign/Slide/2021/Projects/RobotPiano/Slide03/Page12.svelte
+++ src/component/PageDesign/Slide/2021/Projects/RobotPiano/Slide03/Page12.svelte
@@ -14,13 +14,13 @@
             {#if $Device["isMobile"]}
             - 총 두 가지 방식으로 연주 진행<br/>
             &nbsp;&nbsp;1. Normal 방식<br/>
-            &nbsp;&nbsp;2. Snap 방식
+            &nbsp;&nbsp;2. Snap 방식<br/>
             - 두 방식은 전체적인 양상은 비슷하나, 건반을<br/>
             &nbsp;&nbsp;때리는 방식에서 차이가 있음
             {:else}
             - 총 두 가지 방식으로 연주 진행<br/>
             &nbsp;&nbsp;1. Normal 방식<br/>
-            &nbsp;&nbsp;2. Snap 방식
+            &nbsp;&nbsp;2. Snap 방식<br/>
             - 두 방식은 전체적인 양상은 비슷하나, 건반을 때리는 방식에서 차이가 있음
             {/if}
         </p>
