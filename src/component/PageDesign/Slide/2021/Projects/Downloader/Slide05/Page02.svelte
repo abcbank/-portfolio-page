b2a2ebadd204409b295644e1188a1d90a5c1dc50
abcbank <woejr_2012@naver.com>
2023-02-21 11:04:22 +0900

    te

--- src/component/PageDesign/Slide/2021/Projects/Downloader/Slide05/Page02.svelte
+++ src/component/PageDesign/Slide/2021/Projects/Downloader/Slide05/Page02.svelte
@@ -1,14 +1,49 @@
 <script>
-    import { Device, Color, season } from "../../../../../../../global"
+    import { Device } from "../../../../../../../global"
     import DefaultPage from '../../../../defaultPage.svelte'
+    import { fade } from "svelte/transition";
+    import { onMount } from "svelte";
 
     export let color = "transparent";
     export let head = 2;
     export let subhead = 1.5;
     export let context = 1;
-</script>
+    let img;
+	let loaded = false;
+	let failed = false;
+	let loading = false;
+    let src = "http://drive.google.com/uc?export=view&id=1a6fIPrFu7kSj36pkEouYFbF-v0KZdp-n";
 
+    onMount(() => {
+			img = new Image();
+			img.src = src;
+			loading = true;
+
+			img.onload = () => {
+					loading = false;
+					loaded = true;
+			};
+			img.onerror = () => {
+					loading = false;
+					failed = true;
+			};
+    })
+</script>
 <DefaultPage color={color} fontSize={subhead}>
+    <div style="height:100%; width:100%; font-size:{context}rem;">
+    {#if loaded}
+    <img in:fade={{delay:300, duration:300,}} {src} width="auto" height="100%" alt="downloader_total" />
+    {:else if failed}
+        <img  width="100%" height="30%" src="https://icon-library.com/images/not-found-icon/not-found-icon-20.jpg" alt="Not Found" />
+    {:else if loading}
+    <p out:fade={{duration:300,}}>Loading Image...</p>
+    {/if}
+    </div>
 </DefaultPage>
+
 <style>
+    div{
+	    align-items: center;
+		justify-content: center;
+    }
 </style>
