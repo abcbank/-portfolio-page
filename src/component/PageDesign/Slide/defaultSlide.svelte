57c71c2f94855f15903d5261d4ceb1ecb14b431f
abcbank <woejr_2012@naver.com>
2023-02-22 18:34:00 +0900

    te

--- src/component/PageDesign/Slide/defaultSlide.svelte
+++ src/component/PageDesign/Slide/defaultSlide.svelte
@@ -6,7 +6,7 @@
     export let curPage;
     export let maxPage;
     let head = $Device["isSmallScreen"] ? 1 : 2;
-    let subhead = 0.8 * head;
+    let subhead = 1;
     let context = 0.8 * subhead;
     let ButtonType = -1
     let doubleClickTimer;
