fe10c1d83360f11a2d33260e0b10084957317276
abcBank <woejr_2012@naver.com>
2023-05-20 18:13:46 +0900

    te

--- src/component/PageDesign/Slide/2022/Projects/AnalogueCalibration/Slide04/Page06.svelte
+++ src/component/PageDesign/Slide/2022/Projects/AnalogueCalibration/Slide04/Page06.svelte
@@ -11,7 +11,7 @@
 </script>
 
 <DefaultPage color={color} fontSize={subhead}>
-    <p>Photo Sensor</p>wht
+    <p>Photo Sensor</p>
     <div style="font-size:{context}rem; text-align:left; margin-top:20px;">
         <p>
             {#if $Device["isSmallScreen"]}
@@ -22,8 +22,7 @@
             {:else}
             - 지그 하단에서 빛을 받아 빛의 밝기를 수치화<br/>
             - 흑유리들의 포토센서 값과 현재 포토센서의 값을 비교해 현재 차광도를 연산
+            {/if}
         </p>
     </div>
 </DefaultPage>
-<style>
-</style>
